918c71b2be7f7514993152a3547cdd3091781828
catri-01 <catrimaouene@gmail.com>
2025-12-23 20:20:35 +0100

    Deploying my Astro site

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,16 @@ import rss from '@astrojs/rss';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
+    const allPosts = await getCollection('blog');
+    const posts = allPosts.filter(post => post.data.draft !== true);
+
+    return rss({
+        title: SITE_TITLE,
+        description: SITE_DESCRIPTION,
+        site: context.site,
+        items: posts.map((post) => ({
+            ...post.data,
+            link: `/blog/${post.id}/`,
+        })),
+    });
 }
