575f7f97248a87d6592cb9ede6612febf096e011
catri-01 <catrimaouene@gmail.com>
2025-12-23 21:57:53 +0100

    Final fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://blog-software-architect-chancymaouene.vercel.app',
   integrations: [mdx(), sitemap()],
 
   vite: {
